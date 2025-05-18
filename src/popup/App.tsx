85a32b3c3cbd5ad30fe755df66d5576e235dd733
Ilya Sulkhanov <I.Sulkhanov@otpbank.ru>
2025-05-18 12:19:36 +0300

    feat: add popup files

---
+++ src/popup/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Filter Analyzer</h1>
+      {/* TODO: url input */}
+    </div>
+  );
+};
+
+export default App;
